a1f2533efd9ee8e1442f9b8b33a1b08db55f8082
Matt Ward <contact@mwdesigns.com>
2025-07-14 22:55:10 -0700

    [kc] self sign a cert for kubeconfig path

--- pkg/kube-controller/apiserviceproxy/kubeconfig.go
+++ pkg/kube-controller/apiserviceproxy/kubeconfig.go
@@ -2,11 +2,19 @@ package apiserviceproxy
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"log"
+	"math/big"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -39,5 +47,43 @@ func (p *APIServiceProxy) configureKubeconfigProxy(ctx context.Context) error {
 		Transport: transport,
 	}
 
+	cert, err := selfSignCertificate()
+	if err != nil {
+		return fmt.Errorf("failed to self-sign certificate: %w", err)
+	}
+	p.cert = cert
+
 	return nil
+}
+
+func selfSignCertificate() (tls.Certificate, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Apoxy"},
+			CommonName:   "apiserver-proxy.apoxy.dev",
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 24 * 365 * 10), // 10 years
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return tls.Certificate{}, err
+	}
+
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+
+	return tls.X509KeyPair(certPEM, keyPEM)
 }
