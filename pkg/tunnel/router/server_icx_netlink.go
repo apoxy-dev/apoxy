47b2ce69498923aeb84f1c77c862eb2c61010703
Dmitry Ilyevsky <ilyevsky@gmail.com>
2025-10-01 15:29:10 -0700

    [tunnel/alpha] Provide dummy Router implementation for Darwin

--- pkg/tunnel/router/server_icx_netlink.go
+++ pkg/tunnel/router/server_icx_netlink.go
@@ -2,8 +2,40 @@
 
 package router
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"net/netip"
+
+	"github.com/apoxy-dev/apoxy/pkg/tunnel/connection"
+)
 
 func NewICXNetlinkRouter(_ ...Option) (Router, error) {
-	return nil, fmt.Errorf("netlink router is not supported on this platform")
+	return &notImplementedRouter{}, nil
+}
+
+type notImplementedRouter struct{}
+
+func (r *notImplementedRouter) Start(ctx context.Context) error {
+	return errors.New("not implemented")
+}
+
+func (r *notImplementedRouter) AddAddr(_ netip.Prefix, _ connection.Connection) error {
+	return errors.New("not implemented")
+}
+
+func (r *notImplementedRouter) DelAddr(_ netip.Prefix) error {
+	return errors.New("not implemented")
+}
+
+func (r *notImplementedRouter) AddRoute(dst netip.Prefix) error {
+	return errors.New("not implemented")
+}
+
+func (r *notImplementedRouter) DelRoute(dst netip.Prefix) error {
+	return errors.New("not implemented")
+}
+
+func (r *notImplementedRouter) Close() error {
+	return nil
 }
