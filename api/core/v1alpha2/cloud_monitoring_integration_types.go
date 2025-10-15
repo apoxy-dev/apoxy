fd9b9644cfc13477f4d72107f4b5ae6fa5e4e935
Matt Ward <contact@mwdesigns.com>
2025-10-15 12:03:25 -0700

    unused import. oops

--- api/core/v1alpha2/cloud_monitoring_integration_types.go
+++ api/core/v1alpha2/cloud_monitoring_integration_types.go
@@ -1,7 +1,6 @@
 package v1alpha2
 
 import (
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
